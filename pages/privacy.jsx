4652829e8d359988d038535a0a94595ebd585da2
killerclaws12 <78247679+killerclaws12@users.noreply.github.com>
2022-08-02 10:50:37 -0400

    Add files via upload

---
+++ pages/privacy.jsx
@@ -0,0 +1,23 @@
+export default function Privacy() {
+
+    return (
+        <div>
+            <h1 className="font-semibold text-3xl text-white/75">Privacy Policy for Captain</h1>
+            <p className="text-sm text-white/30 mb-4">Last Update: 06/01/2022  🆕️</p>
+  <div className="animateHeader text-white text-opacity-40 mt-5">
+        <p>⊳ To use the CaptainMod, you must define the necessary permissions when adding it to your discord server. <a className="text-white">(Permission=8)</a></p>
+        <div className="py-3"></div>
+        <p>⊳ You can vote for the <a className="text-white" href="https://top.gg/">Captain</a> to support us.</p>
+        <div className="py-3"></div>
+        <p>⊳ Every user can use the CaptainMod however they want, as long as they do not intend to harm.</p>
+        <div className="py-3"></div>
+        <p>⊳ You cannot use certain commands at the same time while using Captain.</p>   
+        <div className="py-3"></div>
+        <p>⊳ CaptainMod does save some data.</p>
+        <div className="py-3"></div>
+        <p>⊳ While using the CaptainMod, your user ID, guild ID and channel ID is saved to our database.</p> 
+    
+        </div>
+            </div>
+    );
+};
---
+++ pages/tos.jsx
@@ -0,0 +1,21 @@
+export default function Tos() {
+  return (
+    <div>
+     <h2 className="mt-4 text-white font-bold text-3xl">Terms of Services</h2>
+      <p className="text-sm text-white/30 mb-1">Last Update: 06/01/2022 🆕️</p>
+  <div className="animateHeader text-white text-opacity-40 mt-5">
+<p>⊳ Every discord server can use the CaptainMod for free, it is completely prohibited to sell or market for a fee.</p>
+<div className="py-3"></div>
+<p>⊳ You cannot use bot commands to harm the bot.</p>
+<div className="py-3"></div>
+<p>⊳ You may not use open source codes to harm other servers.</p>
+<div className="py-3"></div>
+<p>⊳ You may not sell or market open source codes through other platforms.</p>
+<div className="py-3"></div>
+<p>⊳ While using the Captain, you cannot use it in a way that harms other users for troll purposes.</p>
+<div className="py-3"></div>
+<p>⊳ To use certain discord CaptainMod commands, you need to vote for the <a className="text-white" href="https://top.gg/">Captain</a> Mod.</p>
+</div>
+    </div>
+  );
+}
